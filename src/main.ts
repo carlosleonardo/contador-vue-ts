d94320b139c467621eeff30e90283cc52eb4cfa9
Carlos Leonardo <carlos.leonardo.goes@gmail.com>
2025-02-27 16:07:38 -0300

    Limpeza de estilo do main.css e alteração no tema para usar Material

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import PrimeVue from "primevue/config";
-import Aura from "@primeuix/themes/aura";
+import Material from "@primeuix/themes/material";
 
 const app = createApp(App);
 app.use(PrimeVue, {
     theme: {
-        preset: Aura,
+        preset: Material,
     },
 });
 app.mount("#app");
