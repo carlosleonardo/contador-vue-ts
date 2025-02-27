943c0ad91ac11e03f8c6c339c818c66c8dc36f5e
Carlos Leonardo <carlos.leonardo.goes@gmail.com>
2025-02-27 14:48:30 -0300

    Inclusão inicial

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Contador from "./components/Contador.vue";
+</script>
+
+<template>
+    <Contador />
+</template>
+
+<style scoped></style>
---
+++ src/components/Contador.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import Button from "primevue/button";
+
+const contador = ref(0);
+
+function incrementar() {
+    contador.value++;
+}
+function decrementar() {
+    contador.value--;
+}
+function reset() {
+    contador.value = 0;
+}
+</script>
+
+<template>
+    <h1>Contador em: {{ contador }}</h1>
+    <hr />
+    <Button @click="incrementar">Incrementar</Button>
+    <Button @click="decrementar">Decrementar</Button>
+    <Button @click="reset" :disabled="contador === 0">Resetar</Button>
+</template>
+
+<style scoped></style>
